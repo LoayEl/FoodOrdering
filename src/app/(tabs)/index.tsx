3e17b254db4410be450fd1bdcd9a579e77b3c933
1oay E. <elasmarloay@gmail.com>
2024-09-20 22:54:58 -0400

    1. Building UI

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,53 +1,25 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { Image,Text,View, StyleSheet, Platform } from 'react-native';
+import {Colors} from '../../constants/Colors';
+import products from '../../../assets/data/products';
+
+const product=products[1];
 
 import { HelloWave } from '@/src/components/HelloWave';
 import ParallaxScrollView from '@/src/components/ParallaxScrollView';
 import { ThemedText } from '@/src/components/ThemedText';
 import { ThemedView } from '@/src/components/ThemedView';
 
+
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Poop</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style={styles.container}>
+<Image source={{ uri: product.image}}  style={styles.image}/>
+
+          <Text style={styles.title}>{product.name}</Text>
+          <Text style={styles.price}>{product.price}</Text>
+    </View>
+     );
 }
 
 const styles = StyleSheet.create({
@@ -55,6 +27,26 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
+  },
+  container: {
+    backgroundColor:'white',
+    padding:10,
+    borderRadius:1,
+  },
+  title:{
+    fontSize:18, 
+    fontWeight: '600',
+    marginVertical:10,
+
+  },
+  price:{
+    color:Colors.light.tint,
+    fontWeight:'bold'
+  },
+  image:{
+    width: '100%',
+    aspectRatio:1,
+
   },
   stepContainer: {
     gap: 8,
