658f7fa4b61d22739be63be87c7bb0529ecfc218
1oay E. <elasmarloay@gmail.com>
2024-09-21 20:28:10 -0400

    Menu Screen & FlatList

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { View} from 'react-native';
-import products from '../../../assets/data/products';
-import ProductListItem from '../../components/ProductListItem';
-
-
-import { HelloWave } from '@/src/components/HelloWave';
-import ParallaxScrollView from '@/src/components/ParallaxScrollView';
-import { ThemedText } from '@/src/components/ThemedText';
-import { ThemedView } from '@/src/components/ThemedView';
+import { View, FlatList} from 'react-native';
+import products from '@assets/data/products';
+import ProductListItem from '@components/ProductListItem';
 
 export default function MenuScreen() {
-  return (<View> 
-    <ProductListItem product={products[5]}/>
-    <ProductListItem product={products[1]}/>
-    
-  </View>
+  return ( 
+    <FlatList 
+    data={products}
+    renderItem={({item})=> <ProductListItem product={item} />}
+    numColumns={2}
+    contentContainerStyle={{gap:10,padding:10}}
+    columnWrapperStyle={{gap:10}}
+    />
   );
 }
+
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -14,7 +14,11 @@ type ProductListItemProps={
 const ProductListItem=({product}: ProductListItemProps)=>{
   return(
     <View style={styles.container}>
-<Image source={{ uri: product.image|| defaultPizzaImage}}  style={styles.image}/>
+<Image 
+        source={{ uri: product.image|| defaultPizzaImage}}  
+          style={styles.image}
+          resizeMode="contain"
+          />
 
           <Text style={styles.title}>{product.name}</Text>
           <Text style={styles.price}>{product.price}</Text>
@@ -34,7 +38,10 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor:'white',
     padding:10,
-    borderRadius:10,
+    borderRadius:20,
+    flex:1,
+    maxWidth:'50%'
+
   },
   title:{
     fontSize:18, 
