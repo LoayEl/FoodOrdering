38103a78d88be9720b2ae77ac057564cce505595
1oay E. <elasmarloay@gmail.com>
2024-09-23 21:17:31 -0400

    Sharing Data with React Context

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,8 +4,9 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
+import CartProvider from '@/providers/CartProvider';
 import { useColorScheme } from 'hooks/useColorScheme';
+import { Modal } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -28,10 +29,12 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <CartProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="cart" options={{ presentation: 'modal' }} />
       </Stack>
+      </CartProvider>
     </ThemeProvider>
   );
 }
